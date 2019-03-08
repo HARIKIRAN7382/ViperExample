0feab0d179a1d33e55620c8a1260ee8a3a6ea75d
HARI KIRAN REDDY <36834813+HARIKIRAN7382@users.noreply.github.com>
2019-03-08 13:08:55 +0530

    Entity changes
    Did changes on an entity. make complete pattern in viper design pattern

---
+++ ViperExample/ViperExample/Home/Entity/AppColorDao.swift
@@ -0,0 +1,25 @@
+//
+//  AppColorDao.swift
+//  ViperExample
+//
+//  Created by iOS Developer on 28/02/2019 Saka.
+//  Copyright © 1940 iOS Developer. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class AppColorDao{
+    func save(rgb:(CGFloat,CGFloat,CGFloat)) -> (Void){
+        UserDefaults.standard.set(rgb.0, forKey: "Red")
+        UserDefaults.standard.set(rgb.1, forKey: "Green")
+        UserDefaults.standard.set(rgb.2, forKey: "Blue")
+    }
+    func load() -> (CGFloat,CGFloat,CGFloat){
+        let red = CGFloat(UserDefaults.standard.float(forKey: "Red"))
+        let green = CGFloat(UserDefaults.standard.float(forKey: "Green"))
+        let blue = CGFloat(UserDefaults.standard.float(forKey: "Blue"))
+        
+        return (red,green,blue)
+    }
+}
